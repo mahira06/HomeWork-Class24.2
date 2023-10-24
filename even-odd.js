a712d5b214cb140db21a4fb1170d1acd3f0dda1c
mahira06 <122105507+mahira06@users.noreply.github.com>
2023-10-25 00:22:25 +0600

    Add files via upload

---
+++ even-odd.js
@@ -0,0 +1,11 @@
+let number = window.prompt ("Enter a number");
+
+//check if the number is even
+if(number % 2 == 0) {
+    document.write("The number is even.");
+}
+
+// if the number is odd
+else {
+    document.write("The number is odd.");
+}
